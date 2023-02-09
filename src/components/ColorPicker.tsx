73103ba8fbb56cd4cc4e5fd2ba13e8e965e1ccd8
mikan <zuxian.wan@gmail.com>
2023-02-09 19:27:01 +0800

    colorPicker in a popover

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,10 +1,20 @@
 import { HexColorPicker } from "react-colorful";
 import { useState } from 'react';
 
+interface ColorPickerProps {
+  setSelectedColor: Function;
+}
 
-const ColorPicker = () => {
+const ColorPicker = ( props:ColorPickerProps ) => {
+  const { setSelectedColor } = props;
   const [color, setColor] = useState("#aabbcc");
-  return <HexColorPicker color={color} onChange={setColor} />;
+
+  const onColorChange = (value: string) => {
+    setColor(value);
+    setSelectedColor(value);
+  }
+
+  return <HexColorPicker color={color} onChange={onColorChange} />;
 };
 
 export default ColorPicker;
--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -6,7 +6,9 @@ import {
   ProFormSelect,
   ProFormText,
 } from '@ant-design/pro-components';
-import { Button, Form, message, Card } from 'antd';
+import { Button, Form, message, Card, Popover } from 'antd';
+import ColorPicker from './ColorPicker';
+import { useState } from 'react';
 
 const waitTime = (time: number = 100) => {
   return new Promise((resolve) => {
@@ -18,6 +20,12 @@ const waitTime = (time: number = 100) => {
 
 export const NewHabitForm = () => {
   const [form] = Form.useForm<{ name: string; company: string }>();
+  const [MainColor, setMainColor] = useState("#aabbcc");
+  const onColorChange = (value: string) => {
+    setMainColor(value);
+    console.log(MainColor);
+  }
+
   return (
     <ModalForm<{
       name: string;
@@ -47,22 +55,32 @@ export const NewHabitForm = () => {
       onFinish={async (values) => {
         await waitTime(2000);
         console.log(values.name);
-        message.success('提交成功');
+        message.success('Successfully added!');
         return true;
       }}
     >
       <ProForm.Group>
         <ProFormText
-          width="md"
+          width="sm"
           name="name"
-          label="签约客户名称"
-          tooltip="最长为 24 位"
-          placeholder="请输入名称"
+          label="Habit Name"
+          tooltip="Max Length: 24 "
+          placeholder="name of new habit"
         />
 
-        <ProFormText width="md" name="company" label="我方公司名称" placeholder="请输入名称" />
+        <ProFormText width="lg" name="slogan" label="Slogan" placeholder="a slogan to encourage yourself." />
       </ProForm.Group>
       <ProForm.Group>
+        <Form.Item label="MainColor">
+        <Popover placement="bottomLeft" content={<ColorPicker setSelectedColor={onColorChange} />} title="" trigger="click"> 
+            <Button
+              type="text"
+              style={{ backgroundColor: MainColor }}
+            >
+            </Button>
+        </Popover>
+            
+        </Form.Item>
         <ProFormText width="md" name="contract" label="合同名称" placeholder="请输入名称" />
         <ProFormDateRangePicker name="contractTime" label="合同生效时间" />
       </ProForm.Group>
